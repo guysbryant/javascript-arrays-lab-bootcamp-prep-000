159acf6adb0ad39b5d8e2367cd170e29fccb3d03
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-06 19:42:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions and code here
+function destrctivelyAppendKitten(name){
+  
+}
+
+function destrctivelyprePpendKitten(name){
+  
+}
+
+function destrctivelyRemoveLastKitten(name){
+  
+}
+
+function destrctivelyRemoveFirstKitten(name){
+  
+}
